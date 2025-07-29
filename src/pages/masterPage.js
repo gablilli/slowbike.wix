f9726e2247602663afff6ba9fc8bca519675a8e1
gablilli <167802854+gablilli@users.noreply.github.com>
2025-07-29 21:15:14 +0200

    bump to 7.1.0

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,9 +1,7 @@
 $w.onReady(function () {
-  // Carica dinamicamente Lenis da CDN
   const script = document.createElement('script');
   script.src = 'https://unpkg.com/@studio-freight/lenis@1.0.42/dist/lenis.min.js';
   script.onload = () => {
-    // Inizializza Lenis una volta caricato
     const lenis = new Lenis({
       lerp: 0.08,
       smooth: true,
