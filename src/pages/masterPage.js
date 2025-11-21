f38142e8cfc7734d01d256b0aa7bdca065128afe
gablilli <167802854+gablilli@users.noreply.github.com>
2025-11-21 18:07:05 +0100

    fix: console logs for onReady and location change

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -1,7 +1,9 @@
 import wixLocation from 'wix-location';
 
 $w.onReady(() => {
+  console.log('[WIX] onReady executed');
   wixLocation.onChange((location) => {
+    console.log('[WIX] Location changed to:', location.url);
     window.parent.postMessage({ url: location.url }, "*");
   });
 });
